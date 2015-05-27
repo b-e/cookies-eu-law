619d5ed17a53d94ff036815558ce5558a23389b0
Mattia Benedetti <mattiabenedetti@graffiti2000.com>
2015-05-27 10:16:07 +0200

    init

---
+++ startconsent.js
@@ -0,0 +1,33 @@
+$(function ()
+{
+    cc.initialise(
+        {
+            cookies: {
+                social: {},
+                analytics: {},
+                advertising: {}
+               // necessary: {},
+                //mycookieid: {
+                //    title: 'My custom cookie title',
+                //    description: 'Here is a description of '+
+                //    'my custom type of cookie.'
+                //}
+            },
+            settings: {
+                hideallsitesbutton: true,
+                privacyPolicy: true,
+                privacyPolicyUrl: 'http://www.asusplus.it/app/cookie',
+                bannerPosition : 'bottom',
+                //bannerLang : $("html").data("lang")
+            }
+        });
+
+
+    cc.checkapproval();
+
+
+    //cc.onconsent('social', function()
+    //{
+    //    alert("Social cookies have been consented to!");
+    //});
+});
